92334fb6f0dc84d92cd64286ab59ec6baabafe78
Subhasree Lenka <subhasreelenka11@gmail.com>
2026-09-18 16:14:37 +0530

    new update

--- applications/backend-api/api/index.ts
+++ applications/backend-api/api/index.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from '../src/app.module';
-import * as cookieParser from 'cookie-parser';
+import cookieParser from 'cookie-parser';
 
 let cachedApp: any = null;
 
