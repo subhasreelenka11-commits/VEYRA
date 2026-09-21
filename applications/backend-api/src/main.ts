fdbdf57be80b0dfa2f43119538fe4750f0b98e83
Subhasree Lenka <subhasreelenka11@gmail.com>
2026-09-21 16:32:32 +0530

    fix: update CORS for veyraclub domain

--- applications/backend-api/src/main.ts
+++ applications/backend-api/src/main.ts
@@ -20,6 +20,7 @@ async function bootstrap() {
       'http://localhost:3001',
       'http://127.0.0.1:3000',
       'http://127.0.0.1:3001',
+      'https://veyraclub.vercel.app',
     ],
     credentials: true,
   });
